ac2d11854ede4923b74ac812b70381c73e64977b
MrNothing88 <nothingmeallyou@gmail.com>
2025-12-01 14:14:54 -0600

    Initial commit

---
+++ app/src/main/java/com/example/webapp/App.java
@@ -0,0 +1,5 @@
+package com.example.webapp;
+
+import android.app.Application;
+
+public class App extends Application { }
---
+++ app/src/main/java/com/example/webapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.webapp;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView web = findViewById(R.id.webview);
+        web.getSettings().setJavaScriptEnabled(true);
+        web.loadUrl("https://example.com");
+    }
+}
