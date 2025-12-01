1d1c21f5342a32419481a83c1c09bb0d680effc2
MrNothing88 <nothingmeallyou@gmail.com>
2025-12-01 15:30:04 -0600

    Add complete Android WebView app

--- app/src/main/java/com/example/webapp/MainActivity.java
+++ app/src/main/java/com/example/webapp/MainActivity.java
@@ -2,15 +2,18 @@ package com.example.webapp;
 
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        WebView web = new WebView(this);
-        web.getSettings().setJavaScriptEnabled(true);
-        web.loadUrl("https://google.com");
-        setContentView(web);
+        WebView wv = new WebView(this);
+        setContentView(wv);
+
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.setWebViewClient(new WebViewClient());
+        wv.loadUrl("https://google.com"); // Replace with your domain later
     }
 }
